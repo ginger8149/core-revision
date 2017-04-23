3a6a6a292fd46e3e59f94e4cfe1ba3c39f9a40e2
connor spaughton <cs8149@outlok.com>
2017-04-23 13:14:02 +0100

    added mainmenu and started work on math

---
+++ core revision/core revision/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace core_revision
+{
+    [Activity(Label = "core_revision", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+            Button startbutton = FindViewById<Button>(Resource.Id.start);
+
+            startbutton.Click += delegate {
+
+                StartActivity(typeof(home));
+            };
+
+        }
+        
+    }
+}
+
